87e5685ceb7c5523d45776c1a761f0a9e78bb410
Artem Momzikov <artem.momzikov@i.ua>
2021-03-07 18:24:16 +0300

    Input_Card

--- src/App.js
+++ src/App.js
@@ -1,45 +1,13 @@
 import './App.css';
-import React, {Fragment} from 'react';
-import {Table} from './ContactBook/table/table';
-import {AddContactButton} from './ContactBook/add_contact_button/add_contact_button';
+import {Fragment} from 'react';
+import {CardInput} from './Input_Card/card_input';
 
-class App extends React.Component {
-    state = {
-        contacts: [
-            {
-                name: 'Джон',
-                surname: 'Голд',
-                phone: '+80939538673'
-            },
-            {
-                name: 'Тони',
-                surname: 'Старк',
-                phone: '+70684523486',
-            }
-        ],
-    }
-
-    constructor(props) {
-        super(props);
-        this.handleAddContact = this.handleAddContact.bind(this);
-    }
-
-    handleAddContact(contact) {
-        this.state.contacts.push(contact);
-        let addedContact = this.state.contacts;
-        this.setState({
-            contacts: addedContact,
-        });
-    }
-
-    render() {
-        return (
-            <Fragment>
-                <Table contacts={this.state.contacts} />
-                <AddContactButton onAddContact={this.handleAddContact} />
-            </Fragment>
-        );
-    }
+function App() {
+  return (
+    <Fragment>
+        <CardInput />
+    </Fragment>
+  );
 }
 
 export default App;
